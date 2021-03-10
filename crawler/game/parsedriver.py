3ee1e93d07553d6f3a76cedb6a8bcfdecb31c391
JIN TAEYANG <sun@jupiterflow.com>
2021-03-10 14:57:56 +0900

    feat: create new parsedriver

---
+++ crawler/game/parsedriver.py
@@ -0,0 +1,15 @@
+"""
+    Copyright (c) 2021 Aaron(JIN, Taeyang).
+    All rights reserved. This program and the accompanying materials
+    are made available under the terms of the GNU Lesser General Public License v2.1
+    which accompanies this distribution, and is available at
+    https://www.gnu.org/licenses/old-licenses/lgpl-2.1.html
+    
+    Contributors:
+        Aaron(JIN, Taeyang) - Create game/parsedriver
+"""
+
+
+class ParseDriver:
+    def __init__(self):
+        self.agent = 'Game'
