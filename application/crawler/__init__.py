cc256c5685b1d5628e322f90c3c788cf0d9df2b2
JIN TAEYANG <sun@jupiterflow.com>
2021-03-10 10:49:29 +0900

    initial commit for Application and Crawler

---
+++ application/crawler/__init__.py
@@ -0,0 +1,10 @@
+"""
+    Copyright (c) 2021 Aaron(JIN, Taeyang).
+    All rights reserved. This program and the accompanying materials
+    are made available under the terms of the GNU Lesser General Public License v2.1
+    which accompanies this distribution, and is available at
+    http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+    
+    Contributors:
+        Aaron(JIN, Taeyang) - Create Crawler Wrapper Class
+"""
