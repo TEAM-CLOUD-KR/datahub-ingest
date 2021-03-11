88338ea8c1b07eb7f3fb3a358037bedf0118ce51
Taeyang Jin <sun@jupiterflow.com>
2021-03-11 16:46:51 +0900

    feat: class JSON Encoder

---
+++ crawler/jsonencoder/__init__.py
@@ -0,0 +1,18 @@
+"""
+    Copyright (c) 2021 Aaron(JIN, Taeyang).
+    All rights reserved. This program and the accompanying materials
+    are made available under the terms of the GNU Lesser General Public License v2.1
+    which accompanies this distribution, and is available at
+    https://www.gnu.org/licenses/old-licenses/lgpl-2.1.html
+    
+    Contributors:
+        Aaron(JIN, Taeyang) - Class to JSON Encoder
+"""
+
+import json
+from typing import Any
+
+
+class JsonEncoder(json.JSONEncoder):
+    def default(self, o: Any) -> Any:
+        return o.__dict__
