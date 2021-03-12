8df3690e0e50eb9d114da524c47ff6011ec00532
JIN TAEYANG <sun@jupiterflow.com>
2021-03-12 10:08:50 +0900

    refactor: Duplicate Application main class / support s3 upload(well)

--- application/main.py
+++ application/main.py
@@ -4,7 +4,7 @@
     are made available under the terms of the GNU Lesser General Public License v2.1
     which accompanies this distribution, and is available at
     https://www.gnu.org/licenses/old-licenses/lgpl-2.1.html
-    
+
     Contributors:
         Aaron(JIN, Taeyang) - Create Main Application
 """
@@ -12,16 +12,35 @@ from crawler.newsdata import parsedriver as NewsDataParser
 from crawler.opendata import parsedriver as OpenDataParser
 from crawler.gwanbo import parsedriver as GwanboDriver
 from crawler.jsonencoder import JsonEncoder
+from s3client import client as S3Client
 
 from multiprocessing import Pool
+from dateutil.parser import parse
 
 import datetime
 import json
 import os
-import boto3
+
+
+class Application:
+    def __init__(self, parser):
+        self.parser = parser
+
+    def download_and_upload_gwanbo(self, gwanbo: GwanboDriver.GwanboDict):
+        dt = parse(gwanbo.publish["created_at"])
+        directory = os.path.join('data', self.parser.agent, str(dt.year), str(dt.month), str(dt.day))
+
+        self.parser.download_single_gwanbo(gwanbo, directory)
+
+        source_file = os.path.join(directory, gwanbo.id + '.pdf')
+        client = S3Client.Client('', '')
+        client.upload_file('data-portal-cdn', source_file, source_file.replace('\\', '/'))
+
+        os.remove(source_file)
+
 
 if __name__ == '__main__':
-    gwanbo_parser = GwanboDriver.ParseDriver()
+    app = Application(GwanboDriver.ParseDriver())
 
     start_date = datetime.date(2001, 1, 2)  # first: 20010102
     end_date = datetime.date(2021, 3, 11)
@@ -32,27 +51,12 @@ if __name__ == '__main__':
     dates = [str(start_date + datetime.timedelta(x)).replace('-', '')
              for x in range(1, date_gap.days + 1)]
 
-    # dates = ['20210310', '20210311']
-
-    s3 = boto3.client(
-        's3',
-        aws_access_key_id='',
-        aws_secret_access_key=''
-    )
-
-    f_ = os.path.join('data', '2001', '1', '3', '1319593778460000.pdf')
-    bucket = 'data-portal-cdn'
-    key = 'gwanbo/pdf/2001/1/3/1319593778460000.pdf'
-
-    res = s3.upload_file(f_, bucket, key)
-    print(res)
-
-    exit()
+    # dates = ['20210311']
 
     processing_unit = 20
     pool = Pool(processes=processing_unit)
 
-    gwanbo_list = pool.map(gwanbo_parser.get_list_by_date, dates)
+    gwanbo_list = pool.map(app.parser.get_list_by_date, dates)
 
     json_directory = os.path.join('data')
     if not (os.path.isdir(json_directory)):
@@ -62,5 +66,12 @@ if __name__ == '__main__':
     with open(file, 'w', encoding='utf-8') as json_file:
         json.dump(gwanbo_list, fp=json_file, ensure_ascii=False, cls=JsonEncoder)
 
-    result = pool.map(gwanbo_parser.download_multiple_gwanbo, gwanbo_list)
+    download_list = []
+    for gwanbo_item in gwanbo_list:
+        for gwanbo in gwanbo_item:
+            download_list.append(gwanbo)
+
+    result = pool.map(app.download_and_upload_gwanbo, download_list)
     print(result)
+    # result = pool.map(gwanbo_parser.download_multiple_gwanbo, gwanbo_list)
+    # print(result)
