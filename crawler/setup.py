52e150f7c72f6a813430f07b47d1bbae6dedf2d8
Taeyang Jin <sun@jupiterflow.com>
2021-03-13 23:25:57 +0900

    feat: setup.py

---
+++ crawler/setup.py
@@ -0,0 +1,21 @@
+"""
+    Copyright (c) 2021 Aaron(JIN, Taeyang).
+    All rights reserved. This program and the accompanying materials
+    are made available under the terms of the GNU Lesser General Public License v2.1
+    which accompanies this distribution, and is available at
+    https://www.gnu.org/licenses/old-licenses/lgpl-2.1.html
+    
+    Contributors:
+        Aaron(JIN, Taeyang) - 
+"""
+from setuptools import setup
+
+setup(
+    name="crawler",
+    version="1.0.0",
+    description="something..",
+    author="Aaron(JIN, Taeyang)",
+    author_email="sun@jupiterflow.com",
+    url="www.jupiterflow.com",
+    py_modules=['crawler']
+)
